2c4da9a1d275108870e3e0eef21fec543114ae11
mpopovic <m.popovic.hr@gmail.com>
2020-10-29 18:18:18 +0100

    function

--- plot.py
+++ plot.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plotTrig(a):
+    xRange= np.linspace(-np.pi, np.pi, 3000)
+    if a==1:
+        plt.plot(xRange,
+                 np.cos(xRange))
+    if a==2:
+        plt.plot(xRange,
+                 np.sin(xRange))
 
-xRange= np.linspace(-np.pi, np.pi, 3000)
-plt.plot(xRange,
-         np.cos(xRange))
 plt.show()
 
